650bf78e91aaffa79eac2378c84904612d630d34
Guilherme Oliveira <guilhermeoliveira2862@hotmail.com>
2020-11-12 12:21:17 -0300

    container configuration type roles

--- ecs.tf
+++ ecs.tf
@@ -1,36 +1,26 @@
-# ecs.tf
-
+# Creatting Elastic Container Service Cluster
 resource "aws_ecs_cluster" "main" {
   name = "py-cluster"
 }
 
 data "template_file" "py_app" {
-  template = file("./templates/ecs/python_app.json.tpl")
+  template = file("./terraform-templates/ecs/python_app.json.tpl")
 
   vars = {
-    app_image      = var.app_image
-    app_port       = var.app_port
-    fargate_cpu    = var.fargate_cpu
-    fargate_memory = var.fargate_memory
-    aws_region     = var.aws_region
+    app_image      = "417311404467.dkr.ecr.us-west-2.amazonaws.com/guilhermerenew/python-app"
+    app_port       = 8000
+    fargate_cpu    = 1024
+    fargate_memory = 2048
+    aws_region     = "us-west-2"
   }
 }
 
-resource "aws_ecs_task_definition" "app" {
-  family                   = "py-app-task"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.fargate_cpu
-  memory                   = var.fargate_memory
-  container_definitions    = data.template_file.py_app.rendered
-}
-
+# Creating ECS Service 
 resource "aws_ecs_service" "main" {
   name            = "cb-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = var.app_count
+  desired_count   = 3
   launch_type     = "FARGATE"
 
   network_configuration {
@@ -42,8 +32,19 @@ resource "aws_ecs_service" "main" {
   load_balancer {
     target_group_arn = aws_alb_target_group.app.id
     container_name   = "cb-app"
-    container_port   = var.app_port
+    container_port   = 8000
   }
 
   depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+}
+
+# Creating ECS Task Definition
+resource "aws_ecs_task_definition" "app" {
+  family                   = "py-app-task"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = data.template_file.py_app.rendered
 }
